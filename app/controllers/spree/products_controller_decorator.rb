9221e13ff27c34d45a7d23fa8a2b081ef810218a
Janusz Kojro <januszkojro@MacBook-Pro-Janusz.local>
2018-09-23 17:47:57 +0200

    Add button 'Import products from CSV'

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -6,5 +6,6 @@ Spree::ProductsController.class_eval do
       product.shipping_category_id = 1
       product.price = 2.00
       product.save
+      redirect_to admin_products_path
     end
 end
---
+++ app/overrides/add_csv_import_to_product_index.rb
@@ -0,0 +1,9 @@
+Deface::Override.new(virtual_path: 'spree/admin/products/index',
+  name: 'add_csv_upload_to_product_index',
+  insert_after: "erb[loud]:contains('button_link_to')",
+  text: "
+      <%= form_tag import_products_path, multipart: true do %>
+      <%= file_field_tag :file %>
+      <%= submit_tag 'Import products from CSV', { class: 'btn-success', icon: 'add', id: 'admin_new_product' } %>
+    <% end %>
+  ")
