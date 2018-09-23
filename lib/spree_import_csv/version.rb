f2da34fdc58f1fe86c3c8b2d148e541f0539e77e
Janusz Kojro <januszkojro@MacBook-Pro-Janusz.local>
2018-09-23 17:18:14 +0200

    Add extension template

---
+++ lib/spree_import_csv/version.rb
@@ -0,0 +1,18 @@
+module SpreeImportCsv
+  module_function
+
+  # Returns the version of the currently loaded SpreeImportCsv as a
+  # <tt>Gem::Version</tt>.
+  def version
+    Gem::Version.new VERSION::STRING
+  end
+
+  module VERSION
+    MAJOR = 0
+    MINOR = 0
+    TINY  = 1
+    PRE   = 'alpha'.freeze
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+  end
+end
